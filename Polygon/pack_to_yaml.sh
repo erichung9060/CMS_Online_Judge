a38f90abe26e0374d310782407d475596e6453b9
RUEI TING HUNG <101823195+erichung1113@users.noreply.github.com>
2023-07-31 23:51:48 +0800

    Update pack_to_yaml.sh

--- Polygon/pack_to_yaml.sh
+++ Polygon/pack_to_yaml.sh
@@ -6,7 +6,7 @@ if [[ -d "Contest" ]]; then
 fi
 mkdir "Contest"
 
-echo -n "Enter file name or blank for all : "
+echo -n "Enter Problem Name or blank for all : "
 read toDoList
 
 if [[ $toDoList == "" ]];then
@@ -20,53 +20,36 @@ timezone: \"+8\"
 tasks:" > "Contest/contest.yaml"
 
 for PROBLEM_NAME in $toDoList; do
-    if [[ -d "Contest/$PROBLEM_NAME" ]]; then
-        rm -rf Contest/$PROBLEM_NAME
-    fi
-
     mkdir "Contest/$PROBLEM_NAME"
     mkdir "Contest/$PROBLEM_NAME/statement"
     mkdir "Contest/$PROBLEM_NAME/gen"
     mkdir "Contest/$PROBLEM_NAME/input"
     mkdir "Contest/$PROBLEM_NAME/output"
     
-    N=$(ls $PROBLEM_NAME/tests | wc -l)
-    N=$((N/2))
+    N=$(($(ls $PROBLEM_NAME/tests | wc -l)/2))
+    public_tc=$(seq -s, 0 $((N-1)))
+    public_tc=${public_tc%,}
+    PROBLEM_TITTLE=$(cat $PROBLEM_NAME/problem.xml | grep -m 1 'value="[^\"]*"' -o | sed -e s/value=// -e s/\"//g)
+    TIME_LIMIT=$(($(cat $PROBLEM_NAME/problem.xml | grep '<time-limit>' | sed -e s/'<time-limit>'// -e s/'<\/time-limit>'//)/1000))
 
-    for ((i = 1; i <= $N; i++)); do
+    for ((i = 01; i <= $N; i++)); do
         id=$i
-
         if [ $i -lt 10 ]; then
             id="0$i"
         fi
-            
+
         cp "$PROBLEM_NAME/tests/$id" "Contest/$PROBLEM_NAME/input/input$((i-1)).txt"
         cp "$PROBLEM_NAME/tests/$id.a" "Contest/$PROBLEM_NAME/output/output$((i-1)).txt"
     done
-    public_tc=$(seq -s, 0 $((N-1)) )
-    public_tc=${public_tc%,}
-
-    echo -e "name: $PROBLEM_NAME
-title: $PROBLEM_NAME
-time_limit: 1
-memory_limit: 256
-n_input: $N
-token_mode: disabled
-public_testcases: $public_tc
-infile: \"\"
-outfile: \"\"
-    " > Contest/$PROBLEM_NAME/task.yaml
-
-    echo -e "# ST: 100
-$(seq 1 $N)
-    " > Contest/$PROBLEM_NAME/gen/GEN
-
+    
+    echo -e "name: $PROBLEM_NAME\ntitle: $PROBLEM_TITTLE\ntime_limit: $TIME_LIMIT\nmemory_limit: 256\nn_input: $N\ntoken_mode: disabled\npublic_testcases: $public_tc\ninfile: \"\"\noutfile: \"\"" > Contest/$PROBLEM_NAME/task.yaml
+    echo -e "# ST: 100\n$(seq 1 $N)" > Contest/$PROBLEM_NAME/gen/GEN
     echo "statement" > Contest/$PROBLEM_NAME/statement/statement.pdf
-
     echo "  - \"$PROBLEM_NAME\"" >> "Contest/contest.yaml"
 
+    printf "%-3s %ss (%s)\n" "$PROBLEM_NAME" "$TIME_LIMIT" "$PROBLEM_TITTLE"
 done
-echo "users:
-  - username: \"test\"
-    password: \"test\"
-    first_name: \"test\"" >> "Contest/contest.yaml"
+
+echo -e "users:\n  - username: \"test\"\n    password: \"test\"\n    first_name: \"test\"" >> "Contest/contest.yaml"
+echo -e "## CMS Contest import command\n\`\`\`\nsudo cmsImportUser --all -L italy_yaml . && sudo cmsImportContest -i -L italy_yaml .\n\`\`\`" >> "Contest/README.md"
+echo ".DS_Store" >> "Contest/.gitignore"
