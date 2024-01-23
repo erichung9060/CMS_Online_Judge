a91206c1975023d8d60f85d2b99a6b89f815106e
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-01-24 01:24:10 +0800

    Update pack_to_yaml.sh

--- Polygon/pack_to_yaml.sh
+++ Polygon/pack_to_yaml.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# configuration
+Contest_Name="week0"
+Contest_Description="AA競程 - TOI模擬賽 Online Judge"
+#--------------
+
 if [[ -d "Contest" ]]; then
     rm -r Contest    
 fi
@@ -13,8 +18,8 @@ if [[ $toDoList == "" ]];then
     toDoList=$(ls | sed s/pack_to_yaml.sh//g | sed s/Contest//g)
 fi
 
-echo -e "name: \"HSPC-Final\"
-description: \"國立成功大學暑期高中生程式設計邀請賽決賽\"
+echo -e "name: \"$Contest_Name\"
+description: \"$Contest_Description\"
 token_mode: disabled
 timezone: \"+8\"
 tasks:" > "Contest/contest.yaml"
@@ -29,6 +34,7 @@ for PROBLEM_NAME in $toDoList; do
     N=$(($(ls $PROBLEM_NAME/tests | wc -l)/2))
     PROBLEM_TITTLE=$(cat $PROBLEM_NAME/problem.xml | grep -m 1 'value="[^\"]*"' -o | sed -e s/value=// -e s/\"//g)
     TIME_LIMIT=$(($(cat $PROBLEM_NAME/problem.xml | grep '<time-limit>' | sed -e s/'<time-limit>'// -e s/'<\/time-limit>'//)/1000))
+    MEMORY_LIMIT=$(($(cat $PROBLEM_NAME/problem.xml | grep '<memory-limit>' | sed -e s/'<memory-limit>'// -e s/'<\/memory-limit>'//)/1024/1024))
 
     for ((i = 01; i <= $N; i++)); do
         id=$i
@@ -40,12 +46,12 @@ for PROBLEM_NAME in $toDoList; do
         cp "$PROBLEM_NAME/tests/$id.a" "Contest/$PROBLEM_NAME/output/output$((i-1)).txt"
     done
     
-    echo -e "name: $PROBLEM_NAME\ntitle: $PROBLEM_TITTLE\ntime_limit: $TIME_LIMIT\nmemory_limit: 256\nn_input: $N\ntoken_mode: disabled\npublic_testcases: all\ninfile: \"\"\noutfile: \"\"" > Contest/$PROBLEM_NAME/task.yaml
+    echo -e "name: $PROBLEM_NAME\ntitle: $PROBLEM_TITTLE\ntime_limit: $TIME_LIMIT\nmemory_limit: $MEMORY_LIMIT\nn_input: $N\ntoken_mode: disabled\npublic_testcases: all\ninfile: \"\"\noutfile: \"\"" > Contest/$PROBLEM_NAME/task.yaml
     echo -e "# ST: 100\n$(seq 1 $N)" > Contest/$PROBLEM_NAME/gen/GEN
     echo "statement" > Contest/$PROBLEM_NAME/statement/statement.pdf
     echo "  - \"$PROBLEM_NAME\"" >> "Contest/contest.yaml"
 
-    printf "%-3s %ss (%s)\n" "$PROBLEM_NAME" "$TIME_LIMIT" "$PROBLEM_TITTLE"
+    printf "%s  %ds  %dMiB  %s\n" "$PROBLEM_NAME" "$TIME_LIMIT" "$MEMORY_LIMIT" "$PROBLEM_TITTLE" 
 done
 
 echo -e "users:\n  - username: \"test\"\n    password: \"test\"\n    first_name: \"test\"" >> "Contest/contest.yaml"
