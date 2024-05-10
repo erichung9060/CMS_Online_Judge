bacbcad4ef0a55e09a29b61c3b4b4588feffb407
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-05-10 10:15:42 +0800

    annoymous account

--- MyScripts/Account_Generator.cpp
+++ MyScripts/Account_Generator.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define PASSWORD_LENTH 6
+// #define ANNOYMOUS_TEAM_NUMBER 10
 
+#define PASSWORD_LENTH 6
 const char chr[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                     'A', 'B', 'C', 'D', 'E', 'F', 'G',
                     'H', 'I', 'J', 'K', 'L', 'M', 'N',
@@ -37,22 +38,29 @@ int main() {
 
     string prev_teamname, teamname, account, password;
     int Team_Idx = 1;
+
+#ifdef ANNOYMOUS_TEAM_NUMBER
+    while(Team_Idx < ANNOYMOUS_TEAM_NUMBER){
+    teamname = Generate_Account(Team_Idx);
+#else
     while (getline(TeamnameFile, teamname)) {
+#endif
         if(teamname == prev_teamname) {
             AccountFile << account << "\n";
             PasswordFile << password << "\n";
-        }else{
-            account = Generate_Account(Team_Idx);
-            password = Generate_Password();
-            
-            SettingFile << "  - username: \"" << account << "\"\n";
-            SettingFile << "    password: \"" << password << "\"\n";
-            SettingFile << "    first_name: \"" << teamname << "\"\n";
-            AccountFile << account << "\n";
-            PasswordFile << password << "\n";
-            Team_Idx++;
+            continue;
         }
-		prev_teamname = teamname;
+        account = Generate_Account(Team_Idx);
+        password = Generate_Password();
+        
+        SettingFile << "  - username: \"" << account << "\"\n";
+        SettingFile << "    password: \"" << password << "\"\n";
+        SettingFile << "    first_name: \"" << teamname << "\"\n";
+        AccountFile << account << "\n";
+        PasswordFile << password << "\n";
+        Team_Idx++;
+        
+        prev_teamname = teamname;
     }
     TeamnameFile.close();
     PasswordFile.close();
