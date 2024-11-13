3fab8a3e3bf62746775e3cfd18b51e7e2c62cb85
RUEI TING HUNG <101823195+erichung9060@users.noreply.github.com>
2024-11-14 00:23:53 +0800

    Update auto_relaunch.sh

--- MyScripts/auto_relaunch.sh
+++ MyScripts/auto_relaunch.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-id=""
+id="4"
 
-while true; do
-    if ! curl -s http://localhost:8888 > /dev/null || [[ $id != $(cat /home/contest/contest_id) ]]; then
-        id=$(cat /home/contest/contest_id)
-        kill $(ps aux | grep cms | awk '{print $2}' | head -n -1)
-        cmsResourceService -a <<< $id &
-    fi
-    sleep 1
-done
+if ! curl -s http://localhost:8888 > /dev/null; then
+    kill $(ps aux | grep cms | awk '{print $2}' | head -n -1)
+    cmsResourceService -a <<< $id &
+fi
